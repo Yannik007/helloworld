678bc31c6a93f8ebe1d8d5da45eaafb5224c9dae
Yannik-PC\Yannik <994543844@qq.com>
2017-10-09 16:12:19 +0800

    继续完善

--- src/main/java/com.spring/controller/IndexController.java
+++ src/main/java/com.spring/controller/IndexController.java
@@ -1,16 +1,35 @@
 package com.spring.controller;
 
+import com.spring.entity.User;
+import com.spring.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/index")
 public class IndexController {
+    @Autowired
+    UserService userService;
     @RequestMapping("/hello")
     public ModelAndView hello(){
         ModelAndView mav = new ModelAndView();
         mav.setViewName("hello");
         return mav;
+    }
+    @RequestMapping("/getUser")
+    @ResponseBody
+    public List<User> getUser(){
+        List<User> list = null;
+        try {
+            list = userService.getUser();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return  list;
     }
 }
---
+++ src/main/java/com.spring/service/UserService.java
@@ -0,0 +1,11 @@
+package com.spring.service;
+
+import com.spring.entity.User;
+
+import java.util.List;
+
+/*
+*用户接口层 */
+public interface UserService {
+    List<User> getUser()throws Exception;
+}
