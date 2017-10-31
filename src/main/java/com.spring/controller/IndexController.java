9b344d3b290a774c7c1db04f61253396c677c881
tboss <>
2017-11-01 00:03:17 +0800

    elastic增删改查

--- src/main/java/com.spring/controller/IndexController.java
+++ src/main/java/com.spring/controller/IndexController.java
@@ -3,36 +3,49 @@ package com.spring.controller;
 import com.spring.elasticSearch.ElasticSearchClient;
 import com.spring.entity.User;
 import com.spring.service.UserService;
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.cluster.node.DiscoveryNode;
+import org.elasticsearch.common.xcontent.XContentFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/index")
 public class IndexController {
 
-    private static final Logger logger= LoggerFactory.getLogger(IndexController.class);
+    private static final Logger logger = LoggerFactory.getLogger(IndexController.class);
     @Autowired
     private UserService userService;
     @Autowired
     private ElasticSearchClient elasticSearchClient;
+
     @RequestMapping("/hello")
-    public ModelAndView hello(){
+    public ModelAndView hello() {
         ModelAndView mav = new ModelAndView();
         mav.setViewName("hello");
         return mav;
     }
+
     @RequestMapping("/getUser")
     @ResponseBody
-    public List<User> getUser(){
+    public List<User> getUser() {
         List<User> list = null;
         try {
 
@@ -40,16 +53,61 @@ public class IndexController {
         } catch (Exception e) {
             logger.error(e.getMessage(), e);
         }
-        return  list;
+        return list;
     }
-    @RequestMapping("/getClient")
+
+    @RequestMapping(value = "/getClient", method = RequestMethod.GET)
     @ResponseBody
-    public  String getClient(){
+    public List getClient() {
         TransportClient client = elasticSearchClient.getClient();
         List<DiscoveryNode> nodes = client.connectedNodes();
         for (DiscoveryNode node : nodes) {
-            System.out.println(node.getHostAddress()+node.getAddress());
+            System.out.println(node.getAddress());
         }
-        return "hello es";
+        return nodes;
+    }
+
+    @RequestMapping(value = "/addIndex", method = RequestMethod.POST)
+    @ResponseBody
+    public Object addIndex() {
+        Map<String, Object> json = new HashMap<String, Object>();
+        json.put("user", "tyw");
+        json.put("postDate", new Date());
+        json.put("message", "trying add elasticsearch");
+        TransportClient client = elasticSearchClient.getClient();
+        IndexResponse response = client.prepareIndex("apple", "phone", "1").setSource(json).get();
+        return response;
+    }
+
+    @RequestMapping(value = "/updateIndex", method = RequestMethod.POST)
+    @ResponseBody
+    public Object updateIndex() {
+        try {
+            UpdateRequest updateRequest = new UpdateRequest();
+            updateRequest.index("apple");
+            updateRequest.type("phone");
+            updateRequest.id("1");
+            updateRequest.doc(XContentFactory.jsonBuilder()
+                    .startObject()
+                    .field("iphone", "iphone6")
+                    .field("price", "4000")
+                    .endObject());
+            TransportClient client = elasticSearchClient.getClient();
+            UpdateResponse response = client.update(updateRequest).get();
+            return response;
+        } catch (Exception e) {
+            return "false";
+        }
+    }
+    @RequestMapping(value = "/delIndex", method = RequestMethod.DELETE)
+    @ResponseBody
+    public Object delIndex() {
+        try {
+            TransportClient client = elasticSearchClient.getClient();
+            DeleteResponse response = client.prepareDelete("apple","phone","1").get();
+            return response;
+        } catch (Exception e) {
+            return "false";
+        }
     }
 }
